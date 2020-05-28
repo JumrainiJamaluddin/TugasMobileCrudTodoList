393d431ae55266aa8886ddd137c09f581d1490df
JumrainiJamaluddin <jumrainijamaluddin@gmail.com>
2020-05-28 20:30:42 +0800

    create view

--- app/src/main/java/unhas/ac/id/crudtodolist/db/SubscriberRepository.kt
+++ app/src/main/java/unhas/ac/id/crudtodolist/db/SubscriberRepository.kt
@@ -1,4 +1,23 @@
 package unhas.ac.id.crudtodolist.db
 
-class SubscriberRepository {
+class SubscriberRepository(private val dao : SubscriberDAO) {
+
+    val subscriber = dao.getAllSubscribers()
+
+    suspend fun insert(subscriber: Subscriber){
+        dao.insertSubscriber(subscriber)
+    }
+
+    suspend fun update(subscriber: Subscriber){
+        dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber: Subscriber){
+        dao.deleteSubscriber(subscriber)
+    }
+
+    suspend fun deleteAll(){
+        dao.deleteAll()
+    }
+
 }
